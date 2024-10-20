d952bab3a45f1574c0280553cdfd1a4a822dd43e
Leonardo Reis <leonardoreis.ufsj@gmail.com>
2024-10-20 10:31:36 -0300

    Rascunho de layout

--- resources/views/jazigo.blade.php
+++ resources/views/jazigo.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Jazigo Page
+    </x-slot:heading>
     <h1>Hello from the jazigo page</h1>
 </x-layout>
--- resources/views/sepultado.blade.php
+++ resources/views/sepultado.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Sepultado Page
+    </x-slot:heading>
     <h1>Hello from the sepultado page</h1>
 </x-layout>
